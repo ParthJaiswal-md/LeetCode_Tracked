51467c7677ffffed75caeff799f6fe9998b57b81
Parth Jaiswal <parthjaiswal.professional@gmail.com>
2026-07-04 16:51:03 +0530

    Time: 0 ms (100%), Space: 9.8 MB (49.57%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length() != t.length())
+        return false;
+
+    int count[26] = {0};
+    for (int i = 0; i < s.length(); i++)
+    {
+        count[s[i]-97]++;
+    }
+    for (int i = 0; i < t.length(); i++)
+    {
+        count[t[i]-97]--;
+    }
+    for (int i = 0; i < 26; i++)
+    {
+        if(count[i] != 0)
+            return false;
+    }
+    return true;
+    }
+};
